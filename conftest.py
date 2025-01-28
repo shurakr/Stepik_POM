4e8208c4e1e493783ec0ba7e64c6de2e583d4f26
shurakr <shurakr@gmail.com>
2025-01-28 13:27:10 +0300

    Настройка рабочего проекта

---
+++ conftest.py
@@ -0,0 +1,45 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name",
+        action="store",
+        default="chrome",
+        help="Choose browser: chrome or firefox",
+    )
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",
+        help="Choose language: ru, en, fr, es, etc.",
+    )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+    if not user_language:
+        raise pytest.UsageError("--language should be ru, en, fr, es, etc.")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option(
+            "prefs", {"intl.accept_languages": user_language}
+        )
+        options.page_load_strategy = "eager"
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options = webdriver.FirefoxOptions()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_main_page.py
@@ -0,0 +1,26 @@
+import pytest
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_guest_should_see_add_to_basket_button(browser):
+
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    # time.sleep(15)
+
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
+    )
+
+    basket = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert basket, "Кнопка добавления в корзину не найдена"
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
