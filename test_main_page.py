89b27cc6c41ddd999bd22b08b4c0f13bcf645cde
shurakr <shurakr@gmail.com>
2025-01-28 15:25:09 +0300

    Добавлена проверка страница Входа и Регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,32 +1,24 @@
 import pytest
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_guest_should_see_add_to_basket_button(browser):
-
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    # time.sleep(15)
-
-    WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
-    )
-
-    basket = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
-    assert basket, "Кнопка добавления в корзину не найдена"
-
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(
-        browser, link
-    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     page.should_be_login_link()
+
+
+def test_guest_is_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
 
 
 if __name__ == "__main__":
