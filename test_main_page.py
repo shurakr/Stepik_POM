0728ae3182a2ec7cb53b36d23c64205ffe4224bc
shurakr <shurakr@gmail.com>
2025-01-28 15:32:35 +0300

    Дополнение для перехода на страницу Входа и Регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -11,7 +11,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_is_login_page(browser):
