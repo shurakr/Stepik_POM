1b2f593d34ed44df2fb1a5b2ab6b087da74fb53b
shurakr <shurakr@gmail.com>
2025-01-28 14:28:22 +0300

    рефакторинг теста с учетом работы с классами

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 import pytest
-import time
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,12 +19,10 @@ def test_guest_should_see_add_to_basket_button(browser):
     assert basket, "Кнопка добавления в корзину не найдена"
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(
+        browser, link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
